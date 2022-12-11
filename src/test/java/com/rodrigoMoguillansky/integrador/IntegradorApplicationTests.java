1bf1d5451dcd21bd3d159ed90400bc06e9357cdb
romoguill <romoguill@gmail.com>
2022-12-11 18:24:02 -0300

    First commit

---
+++ src/test/java/com/rodrigoMoguillansky/integrador/IntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoMoguillansky.integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
