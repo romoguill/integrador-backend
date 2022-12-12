4d2056b8d112310bb6249d94fd0c25e762345362
romoguill <romoguill@gmail.com>
2022-12-11 21:03:41 -0300

    Finished Odontologo model

--- src/main/java/com/rodrigoMoguillansky/integrador/models/Odontologo.java
+++ src/main/java/com/rodrigoMoguillansky/integrador/models/Odontologo.java
@@ -1,4 +1,27 @@
 package com.rodrigoMoguillansky.integrador.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Odontologos")
+
 public class Odontologo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String matricula;
 }
--- src/main/java/com/rodrigoMoguillansky/integrador/models/Paciente.java
+++ src/main/java/com/rodrigoMoguillansky/integrador/models/Paciente.java
@@ -1,4 +1,21 @@
 package com.rodrigoMoguillansky.integrador.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Pacientes")
+
 public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
